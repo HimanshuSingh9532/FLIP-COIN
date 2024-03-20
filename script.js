5df970652c25d2511ee0ac89aa2fe02c6c022630
Himanshu Singh <shimanshusingh9532@gmail.com>
2024-03-20 13:03:39 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let coin = document.getElementById("co");
+
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+
+
+function change(){
+    alert("hello")
+    coin.classList.add("coin1");
+}
